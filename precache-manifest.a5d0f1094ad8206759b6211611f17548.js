711ce52e66133fe64b9b343736798a6e8ce86220
dima <tynindima@gmail.com>
2020-03-05 16:46:30 +0200

    Updates

---
+++ precache-manifest.a5d0f1094ad8206759b6211611f17548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764e1392e28548c438e81ac734d94248",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc9475a8015a06e79ead",
+    "url": "./static/css/main.adf72ddc.chunk.css"
+  },
+  {
+    "revision": "29a0640c3056cdd0d6b8",
+    "url": "./static/js/2.26a13c39.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.26a13c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9475a8015a06e79ead",
+    "url": "./static/js/main.403e82e5.chunk.js"
+  },
+  {
+    "revision": "acb15a54c7276c556e90",
+    "url": "./static/js/runtime-main.8f6a98c8.js"
+  }
+]);
